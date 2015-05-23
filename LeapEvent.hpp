79a59d8a80f47548b621b5bcf89ee507bd062c79
Ybalrid <ybalrid@cecmu.org>
2015-05-23 18:24:14 +0200

    Add the LeapEvent header that contains the mentioned event system that I forgot to commit because of stupidity

---
+++ LeapEvent.hpp
@@ -0,0 +1,82 @@
+#ifndef LEAP_EVENT
+#define LEAP_EVENT
+
+#include <string>
+#include <AnnEngine.hpp>
+
+using namespace Annwvyn;
+enum LeapEventType
+{
+	HAND_CLOSED
+};
+
+class LeapEvent
+{
+public:
+	LeapEvent()
+	{
+	}
+
+};
+
+///Closing your hand
+class LeapCloseEvent : public LeapEvent
+{
+public:
+	LeapCloseEvent() : LeapEvent()
+	{}
+	bool close;
+	std::string hand;
+};
+
+class LeapEventListener
+{
+public:
+	LeapEventListener()
+	{
+	}
+	
+	bool notifyEvent(LeapEvent* event, LeapEventType eventType)
+	{
+		switch(eventType)
+		{
+		case HAND_CLOSED:
+			return closeEvent(static_cast<LeapCloseEvent*>(event));			
+		}
+
+		return false;
+	}
+
+	virtual bool closeEvent(LeapCloseEvent* e) = 0;
+};
+
+class LeapLoggerListener : public LeapEventListener
+{
+public:
+	LeapLoggerListener() : LeapEventListener()
+	{
+
+	}
+
+	bool closeEvent(LeapCloseEvent* e)
+	{
+		logStream << e->hand << " hand: close -> " << e->close;
+		log();
+		return true;
+	}
+
+	void clearStream()
+	{
+		logStream.str("");
+	}
+
+	void log()
+	{
+		AnnEngine::log(logStream.str());
+		clearStream();
+	}
+	std::stringstream logStream;
+
+};
+
+#endif
